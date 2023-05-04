d7e0fdb47f0ebb25e70ad818595d7f4b78ba0d5b
Kaniel D. Tapper <kanieldannn@gmail.com>
2023-05-04 16:01:07 -0400

    updated dog array

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,23 @@ import './App.css';
 
 function App() {
 
+  const [serverData, setServerData] = React.useState([])
 
+  React.useEffect(() => {
+    fetch("http://44.202.217.194:4000")
+      .then(res => res.json())
+      .then(data => setServerData(data))
+      .catch(err => console.error(err))
+  }, [])
 
 
   return (
     <div className="App">
       <header className="App-header">
         <Hello firstName="Kaniel" lastName="Tapper" age={24} />
+        <Form />
+        {/* @ts-ignore */}
+        {serverData && serverData.map(eachDog => <p>{eachDog.name} - {eachDog.age}</p>)}
       </header>
     </div>
   );
